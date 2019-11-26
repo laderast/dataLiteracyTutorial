26bf05fbd4df6ea5898c13fa0310338cd87bd94a
Ted Laderas <roshioshi@gmail.com>
2019-11-25 16:49:46 -0800

    Create install.R

---
+++ install.R
@@ -0,0 +1,16 @@
+install.packages("learnr")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("shiny")
+install.packages("here")
+install.packages("cowplot")
+install.packages("fivethirtyeight")
+install.packages(c( "plotly",
+    "skimr",
+    "visdat",
+    "NHANES",
+    "rlang",
+    "sf",
+    "leaflet"))
+install.packages("remotes")
+remotes::install_github("laderast/dataLiteracyTutorial")
